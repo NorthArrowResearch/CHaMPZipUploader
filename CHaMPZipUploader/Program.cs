9fdecf7399f6930bdf1e53ff9f82e2ebbdc70c9b
Philip Bailey <philip@northarrowresearch.com>
2017-05-05 10:47:19 -0700

    Visual studio solution

---
+++ CHaMPZipUploader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHaMPZipUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string APIURL = "";
+            string sClientIdentifier = "";
+            string sClientSecret = "";
+            string sPassword = "";
+            string sUserName = "";
+
+            System.IO.FileInfo fiZip = new System.IO.FileInfo("");
+            long nVisitID = 0;
+
+            try
+            {
+                ZipUploader zip = new ZipUploader(APIURL, sClientIdentifier, sClientSecret, sUserName, sPassword);
+
+                zip.Run(fiZip, nVisitID, ZipUploader.CHaMPFileTypes.TopoData);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ CHaMPZipUploader/ZipUploader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHaMPZipUploader
+{
+    public class ZipUploader
+    {
+        public string APIURL { get; internal set; }
+        public string ClientIdentifier { get; internal set; }
+        public string ClientSecret { get; internal set; }
+        public string UserName { get; internal set; }
+        public string Password { get; internal set; }
+
+        public enum CHaMPFileTypes
+        {
+            TopoData
+        }
+
+        public ZipUploader(string sAPIURL, string sClientIdentifer, string sClientSecret, string sUserName, string sPassword)
+        {
+            APIURL = sAPIURL;
+            ClientIdentifier = sClientIdentifer;
+            ClientSecret = sClientSecret;
+            UserName = sUserName;
+            Password = sPassword;
+        }
+
+        public void Run(System.IO.FileInfo fiLocalZip, long visitID, CHaMPFileTypes eType)
+        {
+
+        }
+    }
+}
